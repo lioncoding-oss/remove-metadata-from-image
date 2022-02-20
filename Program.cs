4789e4e5c0aac61facbe45e5e37b820eeb1a2771
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-02-20 01:27:10 +0100

    doc: added README

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// I download the image from https://www.flickr.com/photos/cameralabs/12382975864/in/photostream/
+// This image contains a location info.
+
+using ImageMagick;
+
+using var image = new MagickImage("images/flickr.jpg");
+
+var exifProfile = image.GetExifProfile();
+var iptcProfile = image.GetIptcProfile();
+var xmpProfile = image.GetXmpProfile();
+
+if(exifProfile != null) image.RemoveProfile(exifProfile);
+if(iptcProfile != null) image.RemoveProfile(iptcProfile);
+if(xmpProfile != null) image.RemoveProfile(xmpProfile);
+
+await image.WriteAsync("images/flickr-without-metadata.jpg");
---
+++ README.md
@@ -0,0 +1,13 @@
+## Remove metadata from an image using C#/.NET
+
+I download the image from https://www.flickr.com/photos/cameralabs/12382975864/in/photostream/
+
+This image contains EXIF and IPTC information.
+
+Please note: I have no rights to this image, I'm using this image for demo purposes.
+
+## Result
+
+![](assets/1.png)
+
+![](assets/2.png)
